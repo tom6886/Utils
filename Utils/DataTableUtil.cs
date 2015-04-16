e8da6c3b821701b8d72c9e99ff224338eb7fc11c
tom6886 <478185912@qq.com>
2015-04-16 16:50:12 +0800

    创建项目
    DataTable 获取一列并去重

---
+++ Utils/DataTableUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Data;
+
+namespace Utils
+{
+    public class DataTableUtil
+    {
+        public static object[] GetDistinctValues(DataTable dTable, string colName)
+        {
+            Hashtable hTable = new Hashtable();
+
+            foreach (DataRow drow in dTable.Rows)
+            {
+                try
+                {
+                    hTable.Add(drow[colName].ToString(), string.Empty);
+                }
+                catch { }
+            }
+
+            object[] objArray = new object[hTable.Keys.Count];
+            hTable.Keys.CopyTo(objArray, 0);
+
+            return objArray;
+        }
+    }
+}
