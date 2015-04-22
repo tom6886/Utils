b36fde3a967516552d8419042ede0a55a18dddc9
tom6886 <478185912@qq.com>
2015-04-22 20:44:34 +0800

    添加Pinyin
    封装获取拼音全拼的方法 需要引用微软官方库 ChnCharInfo.dll

---
+++ Utils/Pinyin.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.International.Converters.PinYinConverter;
+using System.Text;
+
+namespace Utils
+{
+    public class Pinyin
+    {
+        /// <summary>
+        /// 汤波
+        /// 2015-4-22 20:41:27
+        /// 获取字符串拼音全拼
+        /// </summary>
+        /// <param name="str">输入字符串</param>
+        /// <returns></returns>
+        public static string GetPinyin(string str)
+        {
+            StringBuilder strB = new StringBuilder();
+
+            char[] tone = { '1', '2', '3', '4' };
+
+            foreach (char item in str.ToCharArray())
+            {
+                if (!ChineseChar.IsValidChar(item))
+                {
+                    strB.Append(item);
+                    continue;
+                }
+
+                ChineseChar c = new ChineseChar(item);
+
+                strB.Append(c.Pinyins[0].TrimEnd(tone));
+            }
+
+            return strB.ToString();
+        }
+    }
+}
