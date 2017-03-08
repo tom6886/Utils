f96774e59fd08b963f5c7145c3da90849e1fa0b2
汤波 <478185912@qq.com>
2017-03-08 16:47:31 +0800

    添加集合转化为DataTable的方法

--- Utils/DataTableUtil.cs
+++ Utils/DataTableUtil.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
+using System.Reflection;
 
 namespace Utils
 {
@@ -19,17 +22,43 @@ namespace Utils
 
             foreach (DataRow drow in dTable.Rows)
             {
-                try
-                {
-                    hTable.Add(drow[colName].ToString(), string.Empty);
-                }
-                catch { }
+                hTable.Add(drow[colName].ToString(), string.Empty);
             }
 
             object[] objArray = new object[hTable.Keys.Count];
+
             hTable.Keys.CopyTo(objArray, 0);
 
             return objArray;
+        }
+
+        /// <summary>
+        /// 汤波
+        /// 2017年3月8日16:44:40
+        /// 将集合转化为DataTable
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(IEnumerable<T> list)
+        {
+            List<PropertyInfo> pList = new List<PropertyInfo>();
+
+            Type type = typeof(T);
+
+            DataTable dt = new DataTable();
+
+            Array.ForEach<PropertyInfo>(type.GetProperties(), p => { pList.Add(p); dt.Columns.Add(p.Name, p.PropertyType); });
+
+            foreach (var item in list)
+            {
+                DataRow row = dt.NewRow();
+
+                pList.ForEach(p => row[p.Name] = p.GetValue(item, null));
+
+                dt.Rows.Add(row);
+            }
+
+            return dt;
         }
     }
 }
